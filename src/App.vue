e03a56e10ce4a2e92341bd193a725542fe1fa0fd
Maud @mihani <maud.remoriquet@gmail.com>
2019-02-13 09:16:08 +0100

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <TodoListTitle titleApp="Todo List App"/>
+        <TodoList />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoListTitle from './components/TodoListTitle.vue';
+    import TodoList from "./components/TodoList";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            TodoList,
+            TodoListTitle
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/TodoListForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <input type="text" name="editItem" v-model="dataItem"/>
+        <input type="submit" name="validItem" @click="validItem()">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoListForm",
+        props:{
+          existingContent: null
+        },
+        methods: {
+            // envoi d'event
+            validItem(){
+                this.$emit('submit',{label:this.dataItem, checked:false});
+                this.dataItem = '';
+            }
+        },
+        data(){
+            return {
+                dataItem: ''
+            }
+        },
+        //Detecte la modification de la donnée
+        watch:{
+            existingContent(){
+                this.dataItem = this.existingContent;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
